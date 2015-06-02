540b8f0c55e5a92297c003017c025823e7b2b9cf
renjunqu <renjunqu@gmail.com>
2015-06-03 02:19:09 +0800

    release 2015-06-03.1

---
+++ src/main/java/com/joymove/concurrent/OrderUUID.java
@@ -0,0 +1,10 @@
+package com.joymove.concurrent;
+
+import org.springframework.stereotype.Component;
+
+/**
+ * Created by jessie on 2015/6/2.
+ */
+@Component("OrderUUID")
+public class OrderUUID {
+}
