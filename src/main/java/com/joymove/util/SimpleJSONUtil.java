ab19131647b49184671af624d78347d585950e65
renjunqu <renjunqu@gmail.com>
2015-06-07 03:50:54 +0800

    2015.6.7.1

---
+++ src/main/java/com/joymove/util/SimpleJSONUtil.java
@@ -0,0 +1,51 @@
+package com.joymove.util;
+
+import com.joymove.entity.JOYNCar;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+
+import java.lang.reflect.Field;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by jessie on 2015/6/6.
+ */
+public class SimpleJSONUtil {
+    public static JSONObject beanToJSONObject(Object beanObject) throws  Exception {
+        JSONObject Reobj = new JSONObject();
+        Field[] fields = beanObject.getClass().getFields();
+        for(Field f : fields) {
+              if(f.get(beanObject)!=null) {
+                  Reobj.put(f.getName(),f.get(beanObject));
+              }
+        }
+        return Reobj;
+    }
+    public static JSONArray listToJSONArray(List beanList) throws Exception {
+        JSONArray reArray = new JSONArray();
+        if(beanList.size()>0) {
+            for(int i=0;i<beanList.size();i++)
+                reArray.add(SimpleJSONUtil.beanToJSONObject(beanList.get(i)));
+        }
+        return reArray;
+    }
+
+    public static void main(String [] args) {
+        List<JOYNCar> ncarList = new ArrayList<JOYNCar>();
+        JOYNCar ncar = new JOYNCar();
+        ncar.vinNum = "sdfdsf";
+        ncarList.add(ncar);
+        ncar = new JOYNCar();
+        ncar.vinNum = "testsetset";
+        ncarList.add(ncar);
+
+        try {
+            JSONObject ttt = new JSONObject();
+           ttt.put("root",SimpleJSONUtil.listToJSONArray(ncarList));
+            System.out.println(ttt);
+        } catch(Exception e){
+            e.printStackTrace();
+        }
+    }
+}
