b04fdbb1eb358f11a73924be76912c01a91b9f57
System Administrator <root@figoxudeMacBook-Pro.local>
2015-06-25 17:46:04 +0800

    2015-06-25.1

---
+++ src/main/java/com/joymove/dao/JOYTestDao.java
@@ -0,0 +1,14 @@
+package com.joymove.dao;
+
+import com.joymove.entity.JOYTest;
+import com.joymove.entity.JOYUser;
+
+import java.util.Map;
+
+/**
+ * Created by qurj on 15/6/25.
+ */
+public interface  JOYTestDao  extends JOYBaseDao<JOYTest>{
+
+
+}
---
+++ src/main/java/com/joymove/entity/JOYTest.java
@@ -0,0 +1,12 @@
+package com.joymove.entity;
+
+/**
+ * Created by qurj on 15/6/25.
+ */
+public class JOYTest  extends  JOYBase {
+
+    public static String tableName = "JOY_Test";
+
+    Integer id;
+    byte[] haha;
+}
